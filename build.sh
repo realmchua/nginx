b92686c4d57dc6582ac4428c270ea2a9909fe281
Realm Chua <105053122+realmchua@users.noreply.github.com>
2022-08-07 17:58:34 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -14,27 +14,31 @@ buildDate() {
     date +%Y.%m.%d.%H.%M
 }
 
-echo "I am checking for any image files."
-result=$(docker images -q realmsg/nginx)
+echo "I am checking for any existing docker image files."
+result=$(cat .build)
 
-if [[ -n "$result" ]]; then
-    echo "Existing images found."
-    echo "" && echo "" && echo ""
-    while true; do
+while true; do
+    if [[ -n "$result" ]]; then
 
+        echo "Existing images found."
+        echo "" && echo "" && echo ""
         read -p "Do you want to proceed? (y/n) " nginx_yn
-
         case $nginx_yn in
         [yY])
+            echo ""
             echo Ok, we will proceed to build the docker image.
-            echo "Please enter your email address for the Automated Certificate Management Environment (ACME)."
-            read ACMEMAIL
             ./cleanup.sh
+            ProceedBuildImage=true
+            echo ""
+            echo "Please enter your email address for the Automated Certificate Management Environment (ACME)."
+            echo ""
+            read ACMEMAIL
             docker build --no-cache=true --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') --build-arg $ACMEMAIL -t realmsg/nginx:$(buildDate) .
-            docker push realmsg/nginx:$(buildDate)
-            break
+            echo realmsg/nginx:$(buildDate) >.build
+            echo "" && echo "" && echo ""
             ;;
         [nN])
+            ProceedBuildImage=false
             echo ""
             echo exiting...
             echo "" && echo "" && echo ""
@@ -42,9 +46,39 @@ if [[ -n "$result" ]]; then
             ;;
         *) echo Invalid Response ;;
         esac
-    done
-else
-    echo "No existing image is found. Proceed to build."
-    docker build --no-cache=true --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') -t realmsg/nginx:$(buildDate) .
-    docker push realmsg/nginx:$(buildDate)
-fi
+    else
+        echo ""
+        echo "No existing docker image is found. Proceed to build."
+        echo ""
+        echo "Please enter your email address for the Automated Certificate Management Environment (ACME)."
+        echo ""
+        read ACMEMAIL
+        docker build --no-cache=true --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') --build-arg $ACMEMAIL -t realmsg/nginx:$(buildDate) .
+        echo realmsg/nginx:$(buildDate) >.build
+        ProceedBuildImage=true
+        echo "" && echo "" && echo ""
+    fi
+    if [[ "$ProceedBuildImage" == "true" ]]; then
+        read -p "Do you want to push this build to Docker Hub? (y/n)" PushToDockerHub_yn
+
+        case $PushToDockerHub_yn in
+
+        [yY])
+            echo "Pushing to DockerHub as $(cat .build)"
+            docker push $(cat .build)
+            echo "" && echo "" && echo ""
+            break
+            ;;
+        [nN])
+            echo "This image will not be push to the DockerHub, to push run the ./push command later on"
+            echo "" && echo "" && echo ""
+            break
+            ;;
+        *)
+            echo ""
+            echo Invalid Response
+            echo "" && echo "" && echo ""
+            ;;
+        esac
+    fi
+done
